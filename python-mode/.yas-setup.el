cd147b15240cf9146770f9367c98eccab89ab8ae
James Ferguson <jferguson@gnubio.com>
2015-07-21 09:22:53 -0400

    Python @param docstrings

--- python-mode/.yas-setup.el
+++ python-mode/.yas-setup.el
@@ -14,12 +14,15 @@
          (max-len (if args (apply 'max (mapcar (lambda (x) (length (nth 0 x))) args)) 0))
          (formatted-args (mapconcat
                 (lambda (x)
-                   (concat (nth 0 x) (make-string (- max-len (length (nth 0 x))) ? ) " -- "
-                           (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
+                  (concat "@param "
+                          (nth 0 x)
+                          (make-string (- max-len (length (nth 0 x))) ? )
+                          " - "
+                          (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
                 args
                 indent)))
     (unless (string= formatted-args "")
-      (mapconcat 'identity (list "Keyword Arguments:" formatted-args) indent))))
+      (mapconcat 'identity (list formatted-args) indent))))
 
 (defun python-args-to-docstring-numpy ()
   "return docstring format for the python arguments in yas-text"
